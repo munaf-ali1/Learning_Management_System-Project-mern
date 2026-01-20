e1339ccd654fcd085ccc0a957ea71bca53c05a35
Munaf Ali <munaf8969924620@gmail.com>
2026-01-20 18:13:53 +0530

    fix backend for vercel

--- backend/index.js
+++ backend/index.js
@@ -1,60 +1,48 @@
-import express from 'express';
-const app = express();
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import authRouter from './routes/auth.routes.js';
-import courseRouter from './routes/course.routes.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import testRouter from './routes/test.routes.js' ;
-import McqRouter from './routes/mcq.routes.js';
-import DsaRouter from './routes/dsa.routes.js';
-
+import authRouter from "./routes/auth.routes.js";
+import courseRouter from "./routes/course.routes.js";
+import testRouter from "./routes/test.routes.js";
+import McqRouter from "./routes/mcq.routes.js";
+import DsaRouter from "./routes/dsa.routes.js";
 
 dotenv.config();
 
-const port = process.env.PORT || 5000;
+const app = express();
+
+
+connectDB();
+
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://codeversefrontend.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
-)
-
+);
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
-
-app.use("/api/auth",authRouter);
-app.use("/api/courses",courseRouter);
-
-app.use("/api/tests",testRouter);
-app.use("/api/mcqs",McqRouter);
+/* ROUTES */
+app.use("/api/auth", authRouter);
+app.use("/api/courses", courseRouter);
+app.use("/api/tests", testRouter);
+app.use("/api/mcqs", McqRouter);
 app.use("/api/dsa", DsaRouter);
 
-
-
-
-
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+/* TEST ROUTE */
+app.get("/", (req, res) => {
+  res.send("Backend live on Vercel 🚀");
 });
 
 
+export default app;
 
-
-
-app.listen(port,()=>{
-    connectDB();
-    console.log(`Server is running on port ${port}`);
-
-})
 
